3db1bc662a10ffb42ea6b1a64f0156b8f42afc9b
carool-santos <caroline.santos@opencircle.com.br>
2022-03-24 10:42:27 -0300

    feat: created schema to employyes

---
+++ src/Schema/employees.schema.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+
+@Schema()
+
+export  class employeeSchema extends Document{
+
+   @Prop()
+   contract_type: string
+
+   @Prop()
+   document: string
+
+   @Prop()
+   name: string
+
+   @Prop()
+   job_Title: string
+
+   @Prop()
+   email: string
+
+   @Prop()
+   telefone_1: string
+
+   @Prop()
+   telefone_2: string
+    
+}
